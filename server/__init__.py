69c7a8dab982e238381b035b0de69e94b08cf345
Thomas Schaper <thomas+uva@libremail.nl>
2016-10-30 01:36:36 +0200

    First simple correct impelementation of the api.
    There is still some work left to make the actual storing and retrieving possible

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from .datastore import SimpleDatastore
+from .session import SimpleSessionstore
 
 app = Flask(__name__)
+datastore = SimpleDatastore()
+sessionstore = SimpleSessionstore()
 
 import server.views
--- tests/helpers.py
+++ tests/helpers.py
@@ -3,13 +3,20 @@ import pytest
 
 
 class MockingFunction():
-    def __init__(self, func=None):
+    def __init__(self, func=None, simple=False, pack=False):
         self.called = False
         self.args = list()
         self.func = func
+        self.simple = simple
+        self.pack = pack
 
     def __call__(self, *args, **kwargs):
         self.called = True
         self.args.append((args, kwargs))
         if self.func is not None:
-            return self.func(*args, **kwargs)
+            if self.simple:
+                return self.func()
+            elif self.pack:
+                return self.func(args)
+            else:
+                return self.func(*args, **kwargs)
