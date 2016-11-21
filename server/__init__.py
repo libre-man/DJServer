81a4b4e27ecdcdff8e56ab40c544bca2c23448f9
Youri Voet <youri@yourivoet.com>
2016-11-21 19:21:41 +0100

    Basic django setup

--- server/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from .datastore import SimpleDatastore
-from .session import SimpleSessionstore
-
-app = Flask(__name__)
-datastore = SimpleDatastore()
-sessionstore = SimpleSessionstore()
-
-import server.views
--- server/cli.py
+++
@@ -1,5 +0,0 @@
-from server import app
-
-
-def main(debug=True):
-    app.run(host='0.0.0.0')
