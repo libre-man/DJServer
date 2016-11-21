7e565c14c22101ffce270e0eb1d3d807b454b471
Youri Voet <youri@yourivoet.com>
2016-11-22 00:15:02 +0100

    Add basic model

--- sdaas/surfer/models.py
+++ sdaas/surfer/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Session:
+    host = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    start = models.DateField()
+    end = models.DateField()
+    created = models.DateField(auto_now_add=True)
+    last_modified = models.DateField(auto_now=True)
+
+
+class Channel:
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+
+
+class Client:
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    created = models.DateField(auto_now_add=True)
+    last_modified = models.DateField(auto_now=True)
+
+
+class Data:
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    client_time = models.DateField()
+    server_time = models.DateField(auto_now_add=True)
