3ed31c35bb1e77b9bc897e930efbe243af3c7a38
Youri Voet <youri@yourivoet.com>
2017-01-16 14:09:07 +0100

    Add collectstatic to deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -37,6 +37,7 @@ echo "Done"
 echo "Applying database migrations"
 cd sdaas
 ./manage.py migrate --settings=sdaas.settings.production
+./manage.py collectstatic --settings=sdaas.settings.production
 echo "Done"
 
 deactivate
