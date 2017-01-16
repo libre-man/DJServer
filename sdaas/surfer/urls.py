9a510a84f55669025d4dd5c1d23358cba59ef263
Youri Voet <youri@yourivoet.com>
2017-01-16 21:56:47 +0100

    Add needed views and urls

--- sdaas/surfer/urls.py
+++ sdaas/surfer/urls.py
@@ -34,4 +34,8 @@ urlpatterns = [
     url(r'^log_data/', views.log_data, name='log_data'),
     url(r'^new_client/', views.new_client, name='new_client'),
     url(r'^join_session/', views.join_session, name='join_session'),
+
+    # Controller->Server API urls:
+    url(r'^im_alive/', views.im_alive, name='im_alive'),
+    url(r'^iteration/', views.iteration, name='iteration'),
 ]
