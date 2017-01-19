65276bd22146a8df17c9c8c1b09d2efe8ee3ceff
Youri Voet <youri@yourivoet.com>
2017-01-19 01:09:06 +0100

    Refactor android api testing

--- sdaas/surfer/tests/test_android_api.py
+++ sdaas/surfer/tests/test_android_api.py
@@ -3,10 +3,11 @@ import json
 
 from django.test import TestCase, Client
 
-from ..testing_utilities import populate_test_db
+from .testing_utilities import populate_test_db
 from surfer import models as s
 
-class RequestTests(TestCase):
+
+class AndroidApiTests(TestCase):
 
     def setUp(self):
         self.client = Client()
@@ -14,33 +15,33 @@ class RequestTests(TestCase):
 
     def test_new_client(self):
         data = {'time': int(time.time()), 'data': {
-                'name': 'testname',
-                'birth_date': int(time.time())
-            }}
+            'birth_year': 1990,
+            'birth_month': 11,
+            'birth_day': 25,
+            'gender': 'f'
+        }}
 
         response = self.client.post('/new_client/', json.dumps(data),
-                content_type='application/json')
-
-        self.assertEqual(len(s.Client.objects.filter(name='testname')), 1)
-        c = s.Client.objects.filter(name='testname').last()
+                                    content_type='application/json')
 
         self.assertEqual(response.status_code, 200)
-
         content = json.loads(response.content.decode('utf-8'))
+        c = s.Client.objects.get(pk=content['client_id'])
+        self.assertIsNotNone(c)
         self.assertTrue(content['success'])
         self.assertEqual(content['client_id'], c.id)
 
     def test_join_session(self):
-        client = s.Client.objects.filter(name='unjoinedclient')[0]
+        client = s.Client.objects.filter(gender='f')[0]
         session = s.Session.objects.filter(name='test_session')[0]
 
         data = {'time': int(time.time()), 'data': {
-                'client_id': client.id,
-                'session_id': session.id
-            }}
+            'client_id': client.id,
+            'session_id': session.id
+        }}
 
         response = self.client.post('/join_session/', json.dumps(data),
-                content_type='application/json')
+                                    content_type='application/json')
 
         self.assertEqual(response.status_code, 200)
 
@@ -50,17 +51,17 @@ class RequestTests(TestCase):
         self.assertEquals(len(content['channels']), 2)
 
     def test_log_data_valid(self):
-        client = s.Client.objects.filter(name='client')[0]
+        client = s.Client.objects.filter(gender='m')[0]
         jclient = s.JoinedClient.objects.filter(client=client)[0]
         session = jclient.session
 
         data = {'time': int(time.time()), 'data': {
-                    'client_id': client.id,
-                    'session_id': session.id
-                }}
+            'client_id': client.id,
+            'session_id': session.id
+        }}
 
         response = self.client.post('/log_data/', json.dumps(data),
-                content_type='application/json')
+                                    content_type='application/json')
 
         self.assertEqual(response.status_code, 200)
 
@@ -69,8 +70,8 @@ class RequestTests(TestCase):
 
     def test_log_data_invalid(self):
         response = self.client.post('/log_data/', '{"time":293084',
-                content_type='application/json')
- 
+                                    content_type='application/json')
+
         self.assertEqual(response.status_code, 200)
 
         content = json.loads(response.content.decode('utf-8'))
@@ -78,10 +79,9 @@ class RequestTests(TestCase):
 
     def test_log_data_empty(self):
         response = self.client.post('/log_data/', '',
-                content_type='application/json')
+                                    content_type='application/json')
 
         self.assertEqual(response.status_code, 200)
 
         content = json.loads(response.content.decode('utf-8'))
         self.assertFalse(content['success'])
-
--- sdaas/surfer/tests/testing_utilities.py
+++ sdaas/surfer/tests/testing_utilities.py
@@ -4,15 +4,16 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from surfer.models import Session, Channel, Client, JoinedClient
 
+
 def populate_test_db():
     """ Add records to the test database. """
-    user = User.objects.create_user('host', 'host@sdaas.nl', 'hostpassword') 
+    user = User.objects.create_user('host', 'host@sdaas.nl', 'hostpassword')
 
     # Create session.
     session_start = timezone.now()
     session_end = session_start + timezone.timedelta(hours=2)
     s = Session(host=user, name='test_session', start=session_start,
-            end=session_end)
+                end=session_end)
     s.save()
 
     # Create channels.
@@ -22,12 +23,11 @@ def populate_test_db():
     c2.save()
 
     # Create client.
-    client1 = Client(name='client', birth_date=timezone.now())
+    client1 = Client(birth_date=timezone.now(), gender='m')
     client1.save()
 
-    client2 = Client(name='unjoinedclient', birth_date=timezone.now())
+    client2 = Client(birth_date=timezone.now(), gender='f')
     client2.save()
 
     joined = JoinedClient(client=client1, session=s)
     joined.save()
-
