7a53133001c2dfc1a8431e642711891471736fc7
Youri Voet <youri@yourivoet.com>
2017-01-20 12:34:12 +0100

    Add name to join session

--- sdaas/surfer/views.py
+++ sdaas/surfer/views.py
@@ -179,8 +179,8 @@ def new_client(request):
                                                      data['birth_day']))
             client.save()
 
-            response_data['success']=True
-            response_data['client_id']=client.id
+            response_data['success'] = True
+            response_data['client_id'] = client.id
 
     return HttpResponse(json.dumps(response_data),
                         content_type='application/json')
@@ -188,37 +188,38 @@ def new_client(request):
 
 @csrf_exempt
 def join_session(request):
-    response_data={}
-    response_data['success']=False
+    response_data = {}
+    response_data['success'] = False
 
     if request.method == 'POST':
-        data, time=utils.parse_client_json(request.body,
+        data, time = utils.parse_client_json(request.body,
                                              {('client_id', int),
                                               ('session_id', int)})
 
         if data is not None and time is not None:
             try:
-                c=Client.objects.get(id=data['client_id'])
-                s=Session.objects.get(id=data['session_id'])
+                c = Client.objects.get(id=data['client_id'])
+                s = Session.objects.get(id=data['session_id'])
 
                 if not JoinedClient.objects.filter(client=c, session=s):
-                    joined_client=JoinedClient(client=c, session=s)
+                    joined_client = JoinedClient(client=c, session=s)
                     joined_client.save()
 
-                response_data['success']=True
-                response_data['session_name']=s.name
-                channels=Channel.objects.filter(session=s)
+                response_data['success'] = True
+                response_data['session_name'] = s.name
+                channels = Channel.objects.filter(session=s)
 
-                response_data['channels']=[]
+                response_data['channels'] = []
                 for c in channels:
                     response_data['channels'].append({'channel_id': c.id,
+                                                      'name': c.name,
                                                       'color': c.color,
                                                       'url': c.url})
 
                 response_data[
-                    'session_start']=utils.datetime_to_epoch(s.start)
+                    'session_start'] = utils.datetime_to_epoch(s.start)
             except ObjectDoesNotExist:
-                response_data['error']='Client or session does not exist'
+                response_data['error'] = 'Client or session does not exist'
 
     return HttpResponse(json.dumps(response_data),
                         content_type='application/json')
@@ -226,14 +227,14 @@ def join_session(request):
 
 @csrf_exempt
 def log_data(request):
-    response_data={}
-    response_data['success']=False
+    response_data = {}
+    response_data['success'] = False
 
     if request.method == 'POST':
-        data, time=utils.parse_client_json(request.body)
+        data, time = utils.parse_client_json(request.body)
 
         if data is not None and time is not None:
-            response_data['success']=True
+            response_data['success'] = True
 
     return HttpResponse(json.dumps(response_data),
                         content_type='application/json')
