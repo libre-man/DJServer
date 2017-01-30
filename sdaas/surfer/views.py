c10b278dd859a10eb1a15c2d2f2f54a8a80d0843
Youri Voet <youri@yourivoet.com>
2017-01-31 00:47:10 +0100

    Start of feedback

--- sdaas/surfer/views.py
+++ sdaas/surfer/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect
 
 from . import utils
-from .models import Client, Session, JoinedClient, Channel, File, ControllerPart, ControllerPartOption, PlayedFile
+from .models import Client, Session, JoinedClient, Channel, File, ControllerPart, ControllerPartOption, PlayedFile, Data
 from .forms import SessionForm, UploadFileForm, ChannelForm, PartSelectForm, PartOptionForm
 
 
@@ -504,6 +504,8 @@ def log_data(request):
         data, time = utils.parse_client_json(request.body)
 
         if data is not None and time is not None:
+            # TODO: log the actual data...
+
             response_data['success'] = True
 
     return HttpResponse(json.dumps(response_data),
@@ -614,7 +616,21 @@ def get_feedback(request):
     """API call from a channel controller to get feedback in a certain
     timeframe.
     """
-    return HttpResponse()
+    if request.method == 'POST':
+        if isinstance(data['start'], int) and isinstance(data['end'], int) and isinstance(data['id'], int):
+            channel = Channel.objects.get(pk=data['id'])
+
+            start = datetime.datetime.utcfromtimestamp(data['start'])
+            end = datetime.datetime.utcfromtimestamp(data['end'])
+
+            feedback = Data.objects.filter(
+                channel=channel, server_time__range=(start, end))
+
+            # TODO: further implement.
+            response = {}
+            response['feedback'] = {}
+
+    return HttpResponse(json.dumps(response), content_type='application/json')
 
 
 @csrf_exempt
